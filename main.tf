0af2dc68fe8c9696c78ac21d9127466159101e16
Flavius Dinu <57911064+flavius-dinu@users.noreply.github.com>
2024-02-21 15:17:51 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    spacelift = {
+      source  = "spacelift-io/spacelift"
+      version = "1.8.0"
+    }
+  }
+}
+
 provider "spacelift" {}
 
 data "spacelift_ips" "ips" {}
